e64453dba386789f981350de78318a10eeba586a
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-07-21 12:28:43 +0200

    App done

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteWasCreated="addQuote"></app-new-quote>
+        <app-quotes-grid :quotes="quotes" @quoteWasDeleted="deleteQuote"></app-quotes-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a Quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuotesGrid from './components/QuotesGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+    
+    export default {
+        data() {
+            return {
+                quotes: ['Life is wonderful'],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            AppQuotesGrid: QuotesGrid,
+            AppNewQuote: NewQuote,
+            AppHeader: Header
+        },
+        methods: {
+            addQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete a quote first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index,1);
+            }
+        }
+        
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuotesGrid.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" :key="index" @click.native="deleteQuote(index)">
+            <p>{{quote}}</p>
+        </app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        },
+        methods: {
+            deleteQuote(id) {
+                this.$emit('quoteWasDeleted', id);
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
